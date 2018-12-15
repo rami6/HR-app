2f3299d49838a6fb92a6ed0d924a0d89ff2e968c
rami6 <30137645+rami6@users.noreply.github.com>
2018-12-14 22:41:25 -0800

    Create models

--- org_structure/models.py
+++ org_structure/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=40, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT, null=True, blank=True)
+    job_title = models.CharField(max_length=20, null=True, blank=True)
+    report_to = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
+    start_date = models.DateField(default=datetime.date.today)
+    left_date = models.DateField(default=datetime.date.today, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
