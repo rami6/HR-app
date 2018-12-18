f64044c1f25ad4f1ff124c7767c49a125fe163ea
rami6 <30137645+rami6@users.noreply.github.com>
2018-12-17 18:51:19 -0800

    Add department-manage view

--- org_structure/urls.py
+++ org_structure/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import EmployeeSearchView
+from .views import EmployeeSearchView, DepartmentManageView
 
 urlpatterns = [
     path('employee-search', EmployeeSearchView.as_view(), name='employee_search'),
+    path('department-manage', DepartmentManageView.as_view(), name='department_manage'),
 ]
--- org_structure/views.py
+++ org_structure/views.py
@@ -1,6 +1,18 @@
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class EmployeeSearchView(LoginRequiredMixin, TemplateView):
     template_name = 'employee_search.html'
+
+
+class DepartmentManageView(LoginRequiredMixin, TemplateView):
+    template_name = 'department_manage.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+        elif not request.user.is_superuser:
+            return HttpResponse('Only superuser can access this page.')
+        return super().dispatch(request, *args, **kwargs)
